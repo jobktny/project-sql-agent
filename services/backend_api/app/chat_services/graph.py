6aadeb8d650b26e2cd8553961070558b88fe6706
Kittinun Yenyueak <kittinun.ye@gmail.com>
2026-01-06 10:52:14 +1030

    add plot agent and refactor graph, services

---
+++ services/backend_api/app/chat_services/graph.py
@@ -0,0 +1,67 @@
+from app.chat_services.agents import (
+    Agent,
+)
+from app.models.state import State
+from fastapi import Depends
+from langgraph.graph import END, StateGraph
+
+
+class GraphBuilder:
+    def __init__(self, agent: Agent = Depends(Agent)) -> None:
+        self.agent = agent
+
+        self.workflow = StateGraph(State)
+
+        # self.workflow.add_node("router", agent.router_agent)
+        self.workflow.add_node("write_query", agent.write_query)
+        self.workflow.add_node("execute_query", agent.execute_query)
+        self.workflow.add_node("generate_answer", agent.generate_answer)
+        self.workflow.add_node("cannot_answer", agent.cannot_answer)
+        self.workflow.add_node("plot_agent", agent.plot_agent)
+        self.workflow.add_node("chat_agent", agent.chat_agent)
+
+        # flow start here
+        self.workflow.set_entry_point("write_query")
+        self.workflow.add_conditional_edges(
+            "write_query",
+            self.chat_router,
+            ["chat_agent", "execute_query", "cannot_answer"],
+        )
+        # self.workflow.add_edge("write_query", "execute_query")
+        self.workflow.add_conditional_edges(
+            "execute_query",
+            self.query_router,
+            ["generate_answer", "cannot_answer", "plot_agent", "write_query"],
+        )
+        self.workflow.add_edge("chat_agent", END)
+        self.workflow.add_edge("plot_agent", END)
+        self.workflow.add_edge("generate_answer", END)
+        self.workflow.add_edge("cannot_answer", END)
+
+        self.workflow = self.workflow.compile()
+
+    def get_graph(self) -> StateGraph:
+        return self.workflow
+
+    def chat_router(self, state: State):
+        if state.chit_chat:
+            return "chat_agent"
+        elif state.out_of_policy:
+            return "cannot_answer"
+        else:
+            return "execute_query"
+
+    def query_router(self, state: State):
+        """Routes to generate_answer, cannot_answer, plot_agent or write_query based on query_execution_status."""
+        if state.sql_query_execution_status == "success":
+            if state.need_visualise:
+                return "plot_agent"
+            else:
+                return "generate_answer"
+
+        elif state.sql_query_execution_status == "failure":
+            if state.sql_error_count < 2:
+                return "write_query"
+
+            else:
+                return "cannot_answer"
